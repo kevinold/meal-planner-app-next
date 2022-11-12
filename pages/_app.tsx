5c73181ac4891d57fb0540770f97aa5b707f392a
Kevin Old <kevin@kevinold.com>
2022-11-12 10:09:35 -0600

    configure amplify withAuthenticator

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@aws-amplify/ui-react/styles.css";
+import { Amplify } from "aws-amplify";
+import type { AppProps } from "next/app";
+import awsExports from "../src/aws-exports";
+import "../styles/globals.css";
+Amplify.configure(awsExports);
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
