8cec4a6b7fcbed834c9e95a15b96fa5c80c6b1d8
Kevin Old <kevin@kevinold.com>
2022-11-12 10:12:28 -0600

    display user and sign out button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
-import { withAuthenticator } from "@aws-amplify/ui-react";
+import { useAuthenticator, withAuthenticator } from "@aws-amplify/ui-react";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
+  const { user, signOut } = useAuthenticator((context) => [context.user]);
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full"></div>
+        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
+          <p>
+            Welcome {user.attributes?.email}
+            <button onClick={signOut}>Sign Out</button>
+          </p>
+        </div>
       </main>
 
       <footer className="flex h-24 w-full items-center justify-center"></footer>
