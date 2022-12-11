0ad9e15c1bd928d2613fe5a2baa73512854a8d2c
Kevin Old <kevin@kevinold.com>
2022-12-10 22:13:03 -0600

    ensure all client side API calls are via Cognito

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,10 @@ import { Amplify } from "aws-amplify";
 import type { AppProps } from "next/app";
 import awsExports from "../src/aws-exports";
 import "../styles/globals.css";
-Amplify.configure(awsExports);
+Amplify.configure({
+  ...awsExports,
+  aws_appsync_authenticationType: "AMAZON_COGNITO_USER_POOLS",
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
