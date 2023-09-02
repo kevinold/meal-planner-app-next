e26f6a06276eec904409e71ed0e48e7524f8d66b
Kevin Old <kevin@kevinold.com>
2023-09-02 15:33:33 -0500

    fix styles for Amplify UI componentsmaking them an import in a separate block so it is not sorted to the top

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
-import "@aws-amplify/ui-react/styles.css";
 import { Amplify } from "aws-amplify";
 import type { AppProps } from "next/app";
 import awsExports from "../src/aws-exports";
 import "../styles/globals.css";
+
+import "@aws-amplify/ui-react/styles.css";
+
 Amplify.configure({
   ...awsExports,
   aws_appsync_authenticationType: "AMAZON_COGNITO_USER_POOLS",
